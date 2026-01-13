0718f845b3f53c2885125ec3af8761deda8dd953
Adam <adampradziad4@gmail.com>
2026-01-13 13:43:40 +0100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Well_Body-app/",
+  base: "/wellBodyApp-Final/",
   plugins: [
     react({
       babel: {
